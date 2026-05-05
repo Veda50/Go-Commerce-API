89dfee73aa3e18f33911e4123efd9f42b6ee7ea4
veda <vedabezaleel@gmail.com>
2026-05-05 23:07:00 +0700

    fix: return 404 instead of 500 on product not found

--- internal/handler/product_handler.go
+++ internal/handler/product_handler.go
@@ -2,10 +2,12 @@ package handler
 
 import (
 	"encoding/json"
+	"errors"
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/user/go-commerce-api/internal/service"
+	"gorm.io/gorm"
 )
 
 type ProductHandler struct {
@@ -32,6 +34,10 @@ func (h *ProductHandler) GetProduct(w http.ResponseWriter, r *http.Request) {
 	id := chi.URLParam(r, "id")
 	product, err := h.productService.GetProduct(id)
 	if err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			http.Error(w, "product not found", http.StatusNotFound)
+			return
+		}
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
